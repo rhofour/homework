44022cfaae10ea4374f4b7d5770be1915875dd4f
Zac Wellmer <zacwellmer@gmail.com>
2017-09-06 14:31:49 +0800

    remove experts. from dumped pickle file name

--- hw1/run_expert.py
+++ hw1/run_expert.py
@@ -63,7 +63,7 @@ def main():
     expert_data = {'observations': np.array(observations),
                     'actions': np.array(actions)}
     
-    with open('{}_{}_data.pkl'.format(module_name, args.num_rollouts), 'wb') as f:
+    with open('{}_{}_data.pkl'.format(module_name.replace('experts.',''), args.num_rollouts), 'wb') as f:
         pickle.dump(expert_data, f)
 
 if __name__ == '__main__':
